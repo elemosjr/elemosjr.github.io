9698babf7c7f3c79aa12a81389b4fbb3ec84ab46
Eduardo Lemos J'unior <elemosjr@gmail.com>
2021-01-11 21:40:50 -0300

    certificados

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-# Apostila de R
-
-[Apostila do curso introdutorio de R](https://estatsej.github.io/apostila_r)
+# elemosjr.github.io
--- _render.R
+++
@@ -1,2 +0,0 @@
-bookdown::render_book("00-index.Rmd", output_dir = "docs") # Renderizar site
-bookdown::render_book("00-index.Rmd", bookdown::pdf_book(), output_dir = "docs") # Renderizar pdf
